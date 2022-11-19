261e29f3362abbd34873a26b5455278e88d744fe
Egor Ivin <ivinegor88@gmail.com>
2022-11-19 03:03:51 +0300

    Add css add new task js

--- app.js
+++ app.js
@@ -23,3 +23,63 @@ const author = "Иван Иванов";
 //Шаблонная строка (литеральная)
 const finish = `${author}:\nзаказал ${projectName} за $${price}`;
 console.log(finish);
+
+/* 
+Васи положил 12 000$ на вклад 7% годовых с капитализацией 1 раз в месяц. Вывести в консоль, сможет ли он купить дом за 13 500$ через 2 года после снятия вклада. И остаток после покупки.
+Итог = Сумма * (1 + Ставка в месяц не в %) ^ срок в месяцах
+*/
+
+const sum = 12000;
+const addProcent = 0.07;
+
+const sumHouse = 13500;
+
+const all = sum * (1 + Number(addProcent) / 12) ** 24;
+console.log(all);
+
+if (all > sumHouse) {
+  console.log(
+    `Вы можете купить себе дом за $${sumHouse}. Ваш остаток: ${all - sumHouse}`
+  );
+} else {
+  console.log(`Вы не можете куить себе дом за $${sumHouse}`);
+}
+
+// switch statement for example
+
+// simple if statement
+const userName = "manager";
+if (userName === "manager") {
+  console.log("You are manager");
+} else if (userName === "admin") {
+  console.log("You are admin");
+} else if (userName === "CEO") {
+  console.log("You are CEO");
+} else {
+  console.log("We not find your position:(");
+}
+
+// switch
+switch (userName) {
+  case "manager": // userName === manager
+    console.log('You are manager! "Switch"');
+    break;
+  case "admin":
+    console.log('You are admin "Switch"');
+    break;
+  case "CEO":
+    console.log('You are CEO "Switch"');
+    break;
+  default:
+    console.log('We not find your position:( "Switch"');
+}
+
+// switch for true / false
+const a = 1;
+switch (true) {
+  case a > 0:
+    console.log("Число положительное"); // true === a > 0 (true)
+    break;
+  case a < 0:
+    console.log("Число отрицательное"); // true !=== a <0 (false)
+}
